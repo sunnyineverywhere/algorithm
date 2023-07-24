6778c30371a61f08bc4a0579608699ce4d84ebf9
HyoJung Yoon <yhjune09@gmail.com>
2023-07-24 18:10:19 +0900

    ✍🏻 docs: add Solution Templete

--- yhjune/solution.py
+++ yhjune/solution.py
@@ -4,6 +4,7 @@
 
 import sys
 
+# 첫 줄에 7, 9 가 입력되고, 뒤의 숫자로 몇 번의 입력이 들어올지 정해지는 경우
 n,m = map(int, sys.stdin.readline().split())
 arr = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
 
@@ -12,3 +13,13 @@ arr = []
 
 for _ in range(m): # m번 loop을 돌면서 input을 arr에 append
     arr.append(list(map(int, input().split())))
+
+# 반복가능한 객체
+item = ["First", "Second", "Third"]
+for i, val in enumerate(item):
+    print("{} 번쨰 값은 {}입니다".format(i, val))
+# 0 번쨰 값은 First입니다
+# 1 번쨰 값은 Second입니다
+# 2 번쨰 값은 Third입니다
+
+answer.add(''.join(result))
